c82f8144a906283979bfe425f891f4602542e06c
scott <liu198143@hotmail.com>
2016-10-13 10:16:26 +0800

    new project momerizewords

---
+++ momorizewords/main.py
@@ -0,0 +1,13 @@
+'''
+Created on Oct 12, 2016
+
+@author: linkcare_l10n_rd
+'''
+
+import pyttsx
+
+if __name__ == '__main__':
+    engine = pyttsx.init()
+    engine.say('anticipate')
+    engine.runAndWait()
+    
